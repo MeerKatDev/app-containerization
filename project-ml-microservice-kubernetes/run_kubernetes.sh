8011da5d484f7b53747dcf5165cdc753086660e3
Luca <lcampobasso@gmail.com>
2020-04-11 12:03:49 +0200

    Task 6 - Deploy with Kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+export dockerpath="lcampobasso/mlapp:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp --port=80 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/mlapp 8000:80
